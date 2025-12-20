5f5f49b04723b4d799988b9eb6ddc5d0b2fbe921
siva2j22 <siva2j22@gmail.com>
2025-12-20 10:51:53 +0530

    files changed

--- data.tf
+++ data.tf
@@ -1,7 +1,18 @@
-# data "aws_availability_zones" "avilable" {
-#     state = "avilable"
-# }
-
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+# then we will get this VPC-ID
+#We can access this vpc_id by data.aws_vpc.default.id then we will get vpc_id
+data "aws_vpc" "default" {
+  default = "true"
+}
+
+data "aws_route_table" "main" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name = "association.main"
+    values = ["true"]
+  }
+}
+
